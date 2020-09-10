0adb3d3ddd94e25cb62ba1abcb09a296ed7d4478
salma soliman <salmasoliman@aucegypt.edu>
2020-09-10 23:47:29 +0200

    f

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -131,7 +131,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/salma/catkin_ws/build/gtest/cmake_install.cmake")
-  include("/home/salma/catkin_ws/build/beginner_tutorials/cmake_install.cmake")
+  include("/home/salma/catkin_ws/build/my_robot/cmake_install.cmake")
 
 endif()
 
